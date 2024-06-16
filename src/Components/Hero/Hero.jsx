1568279d5dcd12b762c0304a675e01b720dd3625
Code With Bikesh <connectedwithme100@gmail.com>
2024-06-16 18:01:32 +0545

    something change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './Hero.css'
 function Hero() {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+     <div className='hero-text'>
+        <h1>We Ensure better education  for a better world</h1>
+        <p>Our cutting-edge curriculum is designed to empower students with the knownledge, sills, and experiences needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore More</button>
+     </div>
     </div>
   )
 }
