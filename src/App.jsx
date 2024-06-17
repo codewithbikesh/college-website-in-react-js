f03d22e8862fdc36843bd442d5125d58e452f810
Code With Bikesh <connectedwithme100@gmail.com>
2024-06-17 13:34:39 +0545

    i have add more content in this page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar  from './Components/Navbar/Navbar'
 import Hero  from './Components/Hero/Hero'
-
-
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Compus from './Components/Compus/Compus'
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className='container'>
+      <Title subTitle='Our PROGRAM' title='What We Offer' />
+      <Programs />
+      <About />
+      <Title subTitle='Gallery' title='Compus Photos' />
+      <Compus />
+      <Title subTitle='TESTIMONIALS' title='What Student Says' />
+      </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  './Navbar.css'
 // if you want to import logo in navbar then you need to import first the logo path from folder
 import logo from '../../assets/logo.png'
 function Navbar() {
+const [sticky, setSticky] = useState(false);
+
+useEffect(() => {
+  window.addEventListener('scroll', () =>{
+    window.scrollY > 50 ? setSticky(true) : setSticky(false)
+  })
+},[]);
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo'></img>
       <ul>
         <li><a  href="#">Home</a></li>
